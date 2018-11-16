80a58718a8225d40c9780fb8660c3a34dcb2d55a
Oleg Shamin <shaminoleg@gmail.com>
2018-11-16 23:43:51 +0100

    Initial commit

---
+++ Asteroids2.0/Application/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  Asteroids2.0
+//
+//  Created by Oleg Shamin on 16/11/2018.
+//  Copyright © 2018 Oleg Shamin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+        ) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+
+}
+
---
+++ Podfile
@@ -0,0 +1,24 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Asteroids2.0' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Asteroids2.0
+
+	pod 'Alamofire', '~> 4.7'
+	pod 'SwiftyJSON'
+	pod 'SwiftLint'
+
+  target 'Asteroids2.0Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Asteroids2.0UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
